d99cd395ea9c07a7380e213ebc4b8f9480608959
Pim Pieters <pim@pieters.tv>
2026-03-11 16:12:59 +0100

    Move token location to Token

--- server/Source/Token.cs
+++ server/Source/Token.cs
@@ -1,15 +1,15 @@
 namespace Server.Tokens;
 
-public abstract class Token(string id)
+public abstract class Token(string id, int x, int y)
 {
     public string id = id;
+    public int x = x;
+    public int y = y;
 }
 
-public class TokenCircle(string id, string color, int x, int y, int r) : Token(id)
+public class TokenCircle(string id, string color, int x, int y, int r) : Token(id, x, y)
 {
     public string type = "circle";
     public string color = color;
-    public int x = x;
-    public int y = y;
     public int r = r;
 }
