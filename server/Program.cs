25507b1c8ffc4570ea9b5e8abd378e2cbad4fa3a
Pim Pieters <pim@pieters.tv>
2026-03-11 14:13:21 +0100

    Add message structs

--- server/Program.cs
+++ server/Program.cs
@@ -7,43 +7,15 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Newtonsoft.Json;
+using Server.Messages;
+using Server.Tokens;
 
 namespace Server;
-
-public record struct DrawingCircle(string id, string color, int x, int y, int r)
-{
-    public string type = "circle";
-    public string id = id;
-    public string color = color;
-    public int x = x;
-    public int y = y;
-    public int r = r;
-}
-
-public class DrawingCircleCreationMessage(DrawingCircle circle)
-{
-    public string type = "create";
-    public DrawingCircle create = circle;
-}
-
-public record struct Move(string id, int x, int y)
-{
-    public string id = id;
-    public int x = x;
-    public int y = y;
-}
-
-public class MoveMessage(Move move)
-{
-    public string type = "move";
-    public Move move = move;
-}
 
 public class Startup
 {
     private readonly ConcurrentDictionary<WebSocket, string> _clients = new();
-    private readonly ConcurrentDictionary<string, DrawingCircle> _circles = new();
-    private int _lastUserId = 0;
+    private readonly ConcurrentDictionary<string, Token> _tokens = new();
 
     public void ConfigureServices(IServiceCollection services) { }
 
@@ -77,31 +49,35 @@ public class Startup
         if (json.type == "request_create")
         {
             // For now, assume all objects will be circles
-            string id = $"object-{Guid.NewGuid().ToString()}";
+            string id = $"object-{Guid.NewGuid()}";
             string color = json.create.color;
             int x = json.create.x;
             int y = json.create.y;
             int r = json.create.r;
 
-            DrawingCircle circle = new(id, color, x, y, r);
-            DrawingCircleCreationMessage creation = new(circle);
-            _circles.TryAdd(id, circle);
-            await BroadcastMessage(JsonConvert.SerializeObject(creation));
+            TokenCircle circle = new(id, color, x, y, r);
+            CreateResponseMessage create = new(circle);
+            _tokens.TryAdd(id, circle);
+            await BroadcastMessage(JsonConvert.SerializeObject(create));
         }
         else if (json.type == "request_move")
         {
             string id = json.move.id;
             int x = json.move.x;
             int y = json.move.y;
-            if (_circles.TryGetValue(id, out DrawingCircle circle))
+            if (_tokens.TryGetValue(id, out Token? token))
             {
-                DrawingCircle newCircle = new(circle.id, circle.color, x, y, circle.r);
-                _circles.AddOrUpdate(
-                    id,
-                    newCircle,
-                    (key, oldValue) => new(oldValue.id, oldValue.color, x, y, oldValue.r)
-                );
-                MoveMessage move = new(new Move(id, x, y));
+                if (token is TokenCircle circle)
+                {
+                    TokenCircle newCircle = new(circle.id, circle.color, x, y, circle.r);
+                    _tokens.AddOrUpdate(id, newCircle, (_, _) => newCircle);
+                }
+                // Unable to match token with any token type, return
+                else
+                {
+                    return;
+                }
+                MoveResponseMessage move = new(new MoveResponseMessage.Move(id, x, y));
                 await BroadcastMessage(JsonConvert.SerializeObject(move));
             }
         }
@@ -110,29 +86,22 @@ public class Startup
     private async Task HandleWebSocket(WebSocket socket)
     {
         // Send the history to the socket
-        foreach (var circle in _circles.Values)
+        foreach (var token in _tokens.Values)
         {
-            DrawingCircleCreationMessage creation = new(circle);
-            await SendMessage(socket, JsonConvert.SerializeObject(creation));
+            CreateResponseMessage create = new(token);
+            await SendMessage(socket, JsonConvert.SerializeObject(create));
         }
 
         var buffer = new byte[1024 * 4];
 
         while (socket.State == WebSocketState.Open)
         {
-            var result = await socket.ReceiveAsync(
-                new ArraySegment<byte>(buffer),
-                CancellationToken.None
-            );
+            var result = await socket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
 
             if (result.MessageType == WebSocketMessageType.Close)
             {
                 // Close the message
-                await socket.CloseAsync(
-                    WebSocketCloseStatus.NormalClosure,
-                    "Closing",
-                    CancellationToken.None
-                );
+                await socket.CloseAsync(WebSocketCloseStatus.NormalClosure, "Closing", CancellationToken.None);
                 _clients.TryRemove(socket, out _);
             }
             else
@@ -150,12 +119,7 @@ public class Startup
         var segment = new ArraySegment<byte>(buffer);
         if (socket.State == WebSocketState.Open)
         {
-            await socket.SendAsync(
-                segment,
-                WebSocketMessageType.Text,
-                true,
-                CancellationToken.None
-            );
+            await socket.SendAsync(segment, WebSocketMessageType.Text, true, CancellationToken.None);
         }
     }
 
---
+++ server/Source/Messages.cs
@@ -0,0 +1,22 @@
+using Server.Tokens;
+
+namespace Server.Messages;
+
+public record struct CreateResponseMessage(Token create)
+{
+    public string type = "create";
+    public Token create = create;
+}
+
+public record struct MoveResponseMessage(MoveResponseMessage.Move move)
+{
+    public string type = "move";
+    public Move move = move;
+
+    public record struct Move(string id, int x, int y)
+    {
+        public string id = id;
+        public int x = x;
+        public int y = y;
+    }
+}
