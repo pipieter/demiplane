ba271daa9d85f4895a6e1bcbdb897da4f427cfa6
Pim Pieters <pim@pieters.tv>
2026-03-11 11:13:57 +0100

    Add circle creation handling

--- server/Program.cs
+++ server/Program.cs
@@ -5,13 +5,30 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Newtonsoft.Json;
 
 namespace Server;
+
+public record struct DrawingCircle(string id, string color, int x, int y, int r)
+{
+    public string type = "circle";
+    public string id = id;
+    public string color = color;
+    public int x = x;
+    public int y = y;
+    public int r = r;
+}
+
+public class DrawingCircleCreation(DrawingCircle circle)
+{
+    public string type = "create";
+    public DrawingCircle create = circle;
+}
 
 public class Startup
 {
     private readonly ConcurrentDictionary<WebSocket, string> _clients = new();
-    private readonly ConcurrentQueue<string> _messages = new();
+    private readonly ConcurrentQueue<DrawingCircle> _circles = new();
     private int _lastUserId = 0;
 
     public void ConfigureServices(IServiceCollection services) { }
@@ -37,20 +54,36 @@ public class Startup
         );
     }
 
+    private async Task ProcessMessage(WebSocket socket, string? message)
+    {
+        if (message == null)
+            return;
+
+        dynamic json = JsonConvert.DeserializeObject(message)!;
+        if (json.type == "request_create")
+        {
+            // For now, assume all objects will be circles
+            string id = $"object-{Guid.NewGuid().ToString()}";
+            string color = json.create.color;
+            int x = json.create.x;
+            int y = json.create.y;
+            int r = json.create.r;
+
+            DrawingCircle circle = new(id, color, x, y, r);
+            DrawingCircleCreation creation = new(circle);
+            _circles.Enqueue(circle);
+            await BroadcastMessage(JsonConvert.SerializeObject(creation));
+        }
+    }
+
     private async Task HandleWebSocket(WebSocket socket)
     {
-        // Stored locally in the thread, in a more robust application
-        // this may be stored in a global dictionary.
-        int userId = Interlocked.Increment(ref _lastUserId);
-
         // Send the history to the socket
-        foreach (var message in _messages)
+        foreach (var circle in _circles)
         {
-            await SendMessage(socket, message);
+            DrawingCircleCreation creation = new(circle);
+            await SendMessage(socket, JsonConvert.SerializeObject(creation));
         }
-
-        // Notify all clients that a new user has joined
-        await BroadcastMessage($"User {userId} has joined the chat!");
 
         var buffer = new byte[1024 * 4];
 
@@ -70,19 +103,18 @@ public class Startup
                     CancellationToken.None
                 );
                 _clients.TryRemove(socket, out _);
-                // Notify all clients that a user has left
-                await BroadcastMessage($"User {userId} has left the chat.");
             }
             else
             {
                 var message = Encoding.UTF8.GetString(buffer, 0, result.Count);
-                await BroadcastMessage($"User {userId}: {message}");
+                await ProcessMessage(socket, message);
             }
         }
     }
 
     private static async Task SendMessage(WebSocket socket, string message)
     {
+        // TODO add a mutex here
         var buffer = Encoding.UTF8.GetBytes(message);
         var segment = new ArraySegment<byte>(buffer);
         if (socket.State == WebSocketState.Open)
@@ -98,8 +130,6 @@ public class Startup
 
     private async Task BroadcastMessage(string message)
     {
-        _messages.Enqueue(message);
-
         foreach (var client in _clients.Keys)
         {
             await SendMessage(client, message);
