088c79d45d367a17f047efd1aeb38a8aed1a1105
DaFrankort <daniel.frankort@telenet.be>
2026-03-11 13:05:32 +0100

    Add .env setup

--- client/src/socket.ts
+++ client/src/socket.ts
@@ -1,15 +1,13 @@
-const BACKEND_URL = "http://localhost:5000"; // TODO store this in an env file
-
-let url;
-
 // Check if currently being hosted in discord as a discord activity
 // If so, we have to use the local /server proxy, as set-up by discord
 // (see the README on how to do this)
+let url;
 if (location.host.includes(".discordsays.com")) {
   url = "/server";
 } else {
   // This is the URL to the backend server
-  url = BACKEND_URL;
+  url = import.meta.env.VITE_SERVER_URL;
+  console.log(url);
 }
 const socket = new WebSocket(url);
 
