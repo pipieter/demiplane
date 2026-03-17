1928a4396649b443746c88edd49159416b37cd16
Pim Pieters <pim@pieters.tv>
2026-03-17 12:33:14 +0100

    Add missing message types

--- server/Source/Server/Messages.cs
+++ server/Source/Server/Messages.cs
@@ -1,8 +1,11 @@
 using Demiplane.Model;
+using Demiplane.Util;
 
 namespace Demiplane.Messages;
 
-public record struct SyncResponseMessage(Token[] tokens, Background background, Grid grid)
+public abstract class Message { }
+
+public class SyncResponseMessage(Token[] tokens, Background background, Grid grid) : Message
 {
     public string type = "sync";
     public Token[] tokens = tokens;
@@ -10,35 +13,79 @@ public record struct SyncResponseMessage(Token[] tokens, Background background, 
     public Grid grid = grid;
 }
 
-public record struct CreateResponseMessage(Token create)
+public class CreateRequestMessage(TokenCreateBody create) : Message
+{
+    public string type = "request_create";
+    public TokenCreateBody create = create;
+}
+
+public class CreateResponseMessage(Token create) : Message
 {
     public string type = "create";
     public Token create = create;
 }
 
-public record struct GridResponseMessage(Grid grid)
+public class GridRequestMessage(Grid grid) : Message
+{
+    public string type = "request_grid";
+    public Grid grid = grid;
+}
+
+public class GridResponseMessage(Grid grid) : Message
 {
     public string type = "grid";
     public Grid grid = grid;
 }
 
-public record struct BackgroundResponseMessage(Background background)
+public class BackgroundRequestMessage(string href) : Message
+{
+    public string type = "request_background";
+    public string href = href;
+}
+
+public class BackgroundResponseMessage(Background background) : Message
 {
     public string type = "background";
     public Background background = background;
 }
 
-public record struct TransformResponseMessage(TransformResponseMessage.Transform transform)
+public record struct Transform(string id, int x, int y, int w, int h)
+{
+    public string id = id;
+    public int x = x;
+    public int y = y;
+    public int w = w;
+    public int h = h;
+}
+
+public class TransformRequestMessage(Transform transform) : Message
+{
+    public string type = "request_transform";
+    public Transform transform = transform;
+}
+
+public class TransformResponseMessage(Transform transform) : Message
 {
     public string type = "transform";
     public Transform transform = transform;
+}
 
-    public record struct Transform(string id, int x, int y, int w, int h)
+public class MessageJsonConverter : Json.TypeConverter<Message>
+{
+    public override Dictionary<string, Type> TypeMap
     {
-        public string id = id;
-        public int x = x;
-        public int y = y;
-        public int w = w;
-        public int h = h;
+        get =>
+            new()
+            {
+                ["sync"] = typeof(SyncResponseMessage),
+                ["grid"] = typeof(GridResponseMessage),
+                ["create"] = typeof(CreateResponseMessage),
+                ["transform"] = typeof(TransformResponseMessage),
+                ["background"] = typeof(BackgroundResponseMessage),
+                ["request_grid"] = typeof(GridRequestMessage),
+                ["request_create"] = typeof(CreateRequestMessage),
+                ["request_transform"] = typeof(TransformRequestMessage),
+                ["request_background"] = typeof(BackgroundRequestMessage),
+            };
     }
 }
