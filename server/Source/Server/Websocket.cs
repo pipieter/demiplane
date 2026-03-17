f1d1d79c42f8537b7bb98cab42b02ac90548b158
Pim Pieters <pim@pieters.tv>
2026-03-17 14:05:52 +0100

    Add Server.Socket class

--- server/Source/Server/Websocket.cs
+++ server/Source/Server/Websocket.cs
@@ -9,48 +9,87 @@ using Newtonsoft.Json;
 
 namespace Demiplane.Server;
 
+public class Socket(WebSocket socket)
+{
+    private readonly WebSocket _socket = socket;
+    private readonly byte[] _buffer = new byte[1024 * 1024 * 100];
+    public bool IsOpen => _socket.State == WebSocketState.Open;
+    public bool WantsToClose { get; private set; }
+
+    public async Task CloseAsync()
+    {
+        await _socket.CloseAsync(WebSocketCloseStatus.NormalClosure, null, CancellationToken.None);
+    }
+
+    public async Task<string> ReceiveAsync()
+    {
+        var response = await _socket.ReceiveAsync(_buffer, CancellationToken.None);
+        if (response.MessageType == WebSocketMessageType.Close)
+            WantsToClose = true;
+
+        var message = Encoding.UTF8.GetString(_buffer, 0, response.Count);
+        return message;
+    }
+
+    public async Task<T> ReceiveAsync<T>()
+    {
+        string message = await ReceiveAsync();
+        T body =
+            Json.Deserialize<T>(message) ?? throw new Exception($"Could not parse {typeof(T).Name} from '{message}'");
+        return body;
+    }
+
+    public async Task SendAsync(string message)
+    {
+        await _socket.SendAsync(
+            Encoding.UTF8.GetBytes(message),
+            WebSocketMessageType.Text,
+            true,
+            CancellationToken.None
+        );
+    }
+
+    public void Dispose()
+    {
+        _socket.Dispose();
+    }
+}
+
 public partial class Server
 {
-    private readonly ConcurrentDictionary<WebSocket, string> _clients = new();
+    private readonly ConcurrentDictionary<Socket, string> _clients = new();
 
     private async Task HandleWebSocket(HttpContext context)
     {
-        WebSocket socket = await context.WebSockets.AcceptWebSocketAsync();
+        WebSocket webSocket = await context.WebSockets.AcceptWebSocketAsync();
+        Socket socket = new(webSocket);
         _clients.TryAdd(socket, "");
 
         // Synchronize the current state
         SyncResponseMessage sync = new([.. _state.Tokens()], _state.GetBackground(), _state.GetGrid());
-        await SendMessage(socket, JsonConvert.SerializeObject(sync));
+        await socket.SendAsync(Json.Serialize(sync));
 
-        // A large buffer is required to upload image data
-        var buffer = new byte[1024 * 1024 * 100];
-
-        while (socket.State == WebSocketState.Open)
+        while (socket.IsOpen)
         {
-            var result = await socket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
-
-            if (result.MessageType == WebSocketMessageType.Close)
+            string message = await socket.ReceiveAsync();
+            if (socket.WantsToClose)
             {
-                // Close the message
-                await socket.CloseAsync(WebSocketCloseStatus.NormalClosure, "Closing", CancellationToken.None);
                 _clients.TryRemove(socket, out _);
+                await socket.CloseAsync();
+                socket.Dispose();
+                return;
             }
             else
             {
-                var message = Encoding.UTF8.GetString(buffer, 0, result.Count);
-                await ProcessMessage(socket, message);
+                try
+                {
+                    await ProcessMessage(socket, message);
+                }
+                catch (Exception exc)
+                {
+                    await socket.SendAsync($"{{\"type\": \"error\", \"message\": \"{exc.Message}\"}}");
+                }
             }
-        }
-    }
-
-    private static async Task SendMessage(WebSocket socket, string message)
-    {
-        // TODO add a mutex here
-        var buffer = System.Text.Encoding.UTF8.GetBytes(message);
-        var segment = new ArraySegment<byte>(buffer);
-        if (socket.State == WebSocketState.Open)
-        {
-            await socket.SendAsync(segment, WebSocketMessageType.Text, true, CancellationToken.None);
         }
     }
 
@@ -58,11 +97,11 @@ public partial class Server
     {
         foreach (var client in _clients.Keys)
         {
-            await SendMessage(client, message);
+            await client.SendAsync(message);
         }
     }
 
-    private async Task ProcessMessage(WebSocket socket, string? message)
+    private async Task ProcessMessage(Socket socket, string? message)
     {
         if (message == null)
             return;
@@ -72,46 +111,42 @@ public partial class Server
         switch (body)
         {
             case CreateRequestMessage create:
-                {
-                    Token? token = Token.Create(create.create);
-                    if (token == null)
-                        return;
+            {
+                Token token = Token.Create(create.create) ?? throw new Exception("Could not create token.");
 
-                    if (!_state.AddToken(token))
-                        return;
+                if (!_state.AddToken(token))
+                    throw new Exception("Could not add token.");
 
-                    CreateResponseMessage response = new(token);
-                    await BroadcastMessage(JsonConvert.SerializeObject(response));
-                    break;
-                }
+                CreateResponseMessage response = new(token);
+                await BroadcastMessage(JsonConvert.SerializeObject(response));
+                break;
+            }
 
             case BackgroundRequestMessage background:
-                {
-                    string href = background.href;
-                    Background? found = Background.FindFromHref(href);
-                    if (found == null)
-                        return;
+            {
+                string href = background.href;
+                Background found = Background.FindFromHref(href) ?? throw new Exception("Resource not found.");
 
-                    _state.SetBackground(found);
-                    BackgroundResponseMessage response = new(_state.GetBackground());
-                    await BroadcastMessage(JsonConvert.SerializeObject(response));
-                    break;
-                }
+                _state.SetBackground(found);
+                BackgroundResponseMessage response = new(_state.GetBackground());
+                await BroadcastMessage(JsonConvert.SerializeObject(response));
+                break;
+            }
 
             case TransformRequestMessage transform:
-                {
-                    string id = transform.transform.id;
-                    int x = transform.transform.x;
-                    int y = transform.transform.y;
-                    int w = transform.transform.w;
-                    int h = transform.transform.h;
-                    if (!_state.TransformToken(id, x, y, w, h))
-                        return;
+            {
+                string id = transform.transform.id;
+                int x = transform.transform.x;
+                int y = transform.transform.y;
+                int w = transform.transform.w;
+                int h = transform.transform.h;
+                if (!_state.TransformToken(id, x, y, w, h))
+                    throw new Exception("Could not transform token.");
 
-                    TransformResponseMessage size = new(new Transform(id, x, y, w, h));
-                    await BroadcastMessage(JsonConvert.SerializeObject(size));
-                    break;
-                }
+                TransformResponseMessage size = new(new Transform(id, x, y, w, h));
+                await BroadcastMessage(JsonConvert.SerializeObject(size));
+                break;
+            }
         }
     }
 }
