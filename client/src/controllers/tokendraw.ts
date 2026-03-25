bd0267d5f16e394a13b20838adc0b52829f05fb1
Pim Pieters <pim@pieters.tv>
2026-03-25 12:56:38 +0100

    Add optimistic messaging to tokendraw

--- client/src/controllers/tokendraw.ts
+++ client/src/controllers/tokendraw.ts
@@ -1,5 +1,7 @@
+import type { TokenCircle, TokenRectangle } from "../models/token";
 import type State from "../state";
 import type Store from "../store";
+import { util } from "../util";
 import type TokenDrawView from "../views/tokendraw";
 import Controller from "./controller";
 
@@ -15,42 +17,55 @@ class TokenDrawController extends Controller<TokenDrawView> {
   }
 
   private createCircle(border: number | null, color: string, x: number, y: number, w: number, h: number) {
-    this.store.send({
-      type: "request_create",
-      create: {
-        type: "circle",
-        id: crypto.randomUUID(),
-        border,
-        color,
-        x,
-        y,
-        w,
-        h,
-        r: 0,
-      },
-    });
+    const circle: TokenCircle = {
+      type: "circle",
+      id: crypto.randomUUID(),
+      border,
+      color,
+      x,
+      y,
+      w,
+      h,
+      r: 0,
+    };
+
+    this.state.createToken(circle);
+    this.store.send({ type: "request_create", create: circle });
   }
 
   private createRectangle(border: number | null, color: string, x: number, y: number, w: number, h: number) {
-    this.store.send({
-      type: "request_create",
-      create: {
-        type: "rectangle",
-        id: crypto.randomUUID(),
-        border,
-        color,
-        x,
-        y,
-        w,
-        h,
-        r: 0,
-      },
-    });
+    const rectangle: TokenRectangle = {
+      type: "rectangle",
+      id: crypto.randomUUID(),
+      border,
+      color,
+      x,
+      y,
+      w,
+      h,
+      r: 0,
+    };
+
+    this.state.createToken(rectangle);
+    this.store.send({ type: "request_create", create: rectangle });
   }
 
   private async createFreedraw(base64: string, x: number, y: number, w: number, h: number) {
-    const href = await this.store.uploadImage(base64);
+    // Create the token locally
+    const { href: localHref } = await util.createLocalImage(base64);
+    this.state.createToken({
+      type: "image",
+      id: crypto.randomUUID(),
+      href: localHref,
+      x,
+      y,
+      w,
+      h,
+      r: 0,
+    });
 
+    // Upload the token to the server
+    const href = await this.store.uploadImage(base64);
     this.store.send({
       type: "request_create",
       create: {
