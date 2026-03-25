3f93c1e343fc50d6e32b65e8481a38b76eb2e28e
Pim Pieters <pim@pieters.tv>
2026-03-25 13:42:07 +0100

    Fix out-of-sync token deletion

--- client/src/controllers/selection.ts
+++ client/src/controllers/selection.ts
@@ -1,3 +1,4 @@
+import type { Token } from "../models/token";
 import type State from "../state";
 import type Store from "../store";
 import type SelectionView from "../views/selection";
@@ -9,10 +10,13 @@ class SelectionController extends Controller<SelectionView> {
 
     this.state.listen("token_select", ([previous, current]) => this.view.select(previous, current));
     this.view.listen("tokens_select", (tokens) => this.state.selectTokens(tokens));
-    this.view.listen("tokens_delete", (tokens) => {
-      const ids = tokens.map((token) => token.id);
-      this.store.send({ type: "request_delete", delete: ids });
-    });
+    this.view.listen("tokens_delete", (tokens) => this.ondelete(tokens));
+  }
+
+  private ondelete(tokens: Token[]) {
+    const ids = tokens.map((token) => token.id);
+    this.state.removeTokens(ids);
+    this.store.send({ type: "request_delete", delete: ids });
   }
 }
 
