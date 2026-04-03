b08371ad761bfff7c8c1f945027885a1157b7aa0
DaFrankort <daniel.frankort@telenet.be>
2026-04-03 15:15:09 +0200

    Add server-status view tests

--- client/tests/views/serverstatus.test.ts
+++ client/tests/views/serverstatus.test.ts
@@ -16,17 +16,58 @@ describe("ServerStatusView", () => {
       expect(view.statusSymbol).toBeDefined();
       expect(view.statusText).toBeDefined();
     });
-  });
 
-  describe("Input Handling", () => {
-    test("PLACEHOLDER", () => {
-      expect(true).toBe(true);
+    test("should initialize in offline state", () => {
+      expect(view.container.className).toBe("server-offline");
     });
   });
 
-  describe("set() method", () => {
-    test("PLACEHOLDER", () => {
-      expect(true).toBe(true);
+  describe("Input Handling", () => {
+    test("should emit 'manual_sync' when clicked while not syncing", () => {
+      view.setOnline();
+
+      view.container.click();
+
+      expect(emitSpy).toHaveBeenCalledWith("manual_sync", { isSyncing: false });
+    });
+
+    test("should NOT emit 'manual_sync' when clicked while already syncing", () => {
+      view.setSyncing();
+
+      view.container.click();
+
+      expect(emitSpy).not.toHaveBeenCalled();
+    });
+  });
+
+  describe("State Methods", () => {
+    test("setSyncing() updates UI and sets cursor to progress", () => {
+      view.setSyncing();
+
+      expect(view.container.className).toBe("server-syncing");
+      expect(view.statusSymbol.className).toContain("fa-rotate");
+      expect(view.statusText.textContent).toBe("Syncing");
+      expect(document.body.style.cursor).toBe("progress");
+    });
+
+    test("setOnline() updates UI and resets cursor", () => {
+      view.setOnline();
+
+      expect(view.container.className).toBe("server-online");
+      expect(view.statusSymbol.className).toContain("fa-check");
+      expect(view.statusText.textContent).toContain("Online");
+      expect(document.body.style.cursor).toBe("default");
+    });
+
+    test("setOffline() updates UI and resets cursor", () => {
+      view.setSyncing(); // Set different state first to ensure we're not using default.
+
+      view.setOffline();
+
+      expect(view.container.className).toBe("server-offline");
+      expect(view.statusSymbol.className).toContain("fa-xmark");
+      expect(view.statusText.textContent).toBe("Offline");
+      expect(document.body.style.cursor).toBe("default");
     });
   });
 });
