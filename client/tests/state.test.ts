81c97335af35120fc2867f63dfea7bc725c97dd4
DaFrankort <daniel.frankort@telenet.be>
2026-04-03 11:06:45 +0200

    Add state-transform test

--- client/tests/state.test.ts
+++ client/tests/state.test.ts
@@ -1,6 +1,7 @@
 import { beforeEach, describe, expect, test, vi } from "vitest";
 import State from "../src/state";
 import { mockUser, tokenMock } from "./mocking";
+import { Transform } from "../src/models/transform";
 
 describe("State Class", () => {
   let state: State;
@@ -38,6 +39,35 @@ describe("State Class", () => {
       expect(state.getTokens()).not.toContain(token1);
       expect(deleteSpy).toHaveBeenCalledWith([token1.id]);
       expect(selectSpy).toHaveBeenCalledWith([[token1], []]);
+    });
+
+    test("Should transform a token and emit token_transform", () => {
+      const token = tokenMock.getImage();
+      state.createToken(token);
+
+      const transform: Transform = {
+        id: token.id,
+        name: "Edited Name",
+        x: token.x + 10,
+        y: token.y + 20,
+        w: token.w + 30,
+        h: token.h + 40,
+        r: token.r + 50,
+      };
+
+      const spy = vi.fn();
+      state.listen("token_transform", spy);
+
+      state.transformToken(transform);
+
+      const updatedToken = state.getTokens()[0];
+      expect(spy).toHaveBeenCalledWith([token, transform]);
+      expect(updatedToken.name).toBe(transform.name);
+      expect(updatedToken.x).toBe(transform.x);
+      expect(updatedToken.y).toBe(transform.y);
+      expect(updatedToken.w).toBe(transform.w);
+      expect(updatedToken.h).toBe(transform.h);
+      expect(updatedToken.r).toBe(transform.r);
     });
   });
 
