87b9258db26505f561db887b1f4ef7536e431dcb
DaFrankort <daniel.frankort@telenet.be>
2026-04-08 10:09:38 +0200

    Refactor UserCursorsController to use send-cooldown

--- client/src/controllers/usercursors.ts
+++ client/src/controllers/usercursors.ts
@@ -5,12 +5,8 @@ import type UserCursorsView from "../views/usercursors";
 import Controller from "./controller";
 
 class UserCursorController extends Controller<UserCursorsView> {
-  private readonly timeBetweenMessages = 50; // in milliseconds
-  private timeSinceLastMessage: number;
-
   constructor(store: Store, state: State, view: UserCursorsView) {
     super(store, state, view);
-    this.timeSinceLastMessage = 0;
 
     this.state.listen("user_change", (user) => {
       if (this.state.isMe(user)) return;
@@ -27,19 +23,16 @@ class UserCursorController extends Controller<UserCursorsView> {
   }
 
   private emitPosition(position: Point | null) {
-    const now = Date.now();
-    if (now - this.timeSinceLastMessage < this.timeBetweenMessages) {
-      return;
-    }
-    this.timeSinceLastMessage = now;
-
     const secret = this.store.getSecretToken();
     if (!secret) return;
 
-    this.store.send({
-      type: "request_user_position",
-      user: { secret, position },
-    });
+    this.store.send(
+      {
+        type: "request_user_position",
+        user: { secret, position },
+      },
+      50,
+    );
   }
 
   updatePosition(evt: MouseEvent) {
