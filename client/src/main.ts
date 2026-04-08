2a75916a45620f31b262cca9623ca9c8fa37fed0
Pim Pieters <pim@pieters.tv>
2026-04-08 14:20:41 +0200

    Move cursor position handling to user cursors view

--- client/src/main.ts
+++ client/src/main.ts
@@ -43,7 +43,7 @@ const headerView = new SidebarView();
 const tokenEditView = new TokenEditView();
 const tokenListView = new TokenListView();
 const userView = new UserView();
-const userCursorView = new UserCursorsView();
+const userCursorView = new UserCursorsView(state.grid);
 const hoverView = new HoverView();
 
 new BackgroundController(store, state, backgroundView);
--- client/src/views/usercursors.ts
+++ client/src/views/usercursors.ts
@@ -1,10 +1,29 @@
+import { Listener } from "../listener";
+import type Grid from "../models/grid";
+import type { Point } from "../models/transform";
 import type { User } from "../models/user";
 
-class UserCursorsView {
-  public readonly layer: HTMLElement;
+interface UserCursorsMap {
+  cursor_change: Point | null;
+}
 
-  constructor() {
+class UserCursorsView extends Listener<UserCursorsMap> {
+  public readonly layer: HTMLElement;
+  private readonly grid: Grid;
+
+  constructor(grid: Grid) {
+    super();
+    this.grid = grid;
     this.layer = document.getElementById("user-cursors") as unknown as HTMLElement;
+
+    document.body.addEventListener("pointermove", (evt) => {
+      const position = this.grid.getCoordinates(evt);
+      this.emit("cursor_change", position);
+    });
+
+    document.body.addEventListener("pointerout", () => {
+      this.emit("cursor_change", null);
+    });
   }
 
   renderUserCursor(user: User) {
