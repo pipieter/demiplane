c90f29bf243e79947fea3cf12f42bee58e610b27
Pim Pieters <pim@pieters.tv>
2026-05-10 17:30:37 +0200

    Improve background sidebar styling

--- client/src/views/backgroundlist.ts
+++ client/src/views/backgroundlist.ts
@@ -1,4 +1,6 @@
+import AutosizeInput from "../components/AutosizeInput";
 import { Listener } from "../listener";
+import type { BackgroundLayer } from "../models/background";
 import type Background from "../models/background";
 
 interface BackgroundListViewMap {
@@ -32,23 +34,49 @@ class BackgroundListView extends Listener<BackgroundListViewMap> {
     // Iterate in reverse order
     const layers = [...background.layers].reverse();
     for (const layer of layers) {
-      const div = document.createElement("div");
-      const name = document.createElement("p");
+      const li = document.createElement("li");
+      li.id = `background-layer-${layer.id}`;
+      li.style.display = "flex";
+      li.style.justifyContent = "space-between";
+      li.addEventListener("click", () => this.emit("background_layer_select", layer.id));
 
-      div.id = `background-layer-${layer.id}`;
-      div.onclick = () => this.emit("background_layer_select", layer.id);
-      name.innerText = layer.name;
-
-      if (layer.id === background.selected) {
-        name.style.color = "red";
+      if (background.selected === layer.id) {
+        li.classList.add("selected");
       }
 
-      div.appendChild(name);
-      this.layersDiv.appendChild(div);
+      const nameInput = AutosizeInput(layer.name);
+      nameInput.type = "text";
+      nameInput.classList.add("token-name-input");
+      nameInput.maxLength = 36;
+      nameInput.minLength = 1;
+
+      nameInput.addEventListener("click", (e) => e.stopPropagation());
+      nameInput.addEventListener("change", () => this.onRename(layer, nameInput));
+      nameInput.addEventListener("keydown", (e) => {
+        if (e.key === "Enter") nameInput.blur(); // Trigger change-event
+      });
+
+      const deleteIcon = document.createElement("i");
+      deleteIcon.classList.add("fa-solid", "fa-trash");
+      deleteIcon.onclick = () => this.emit("background_layer_delete", layer.id);
+
+      li.onclick = () => this.emit("background_layer_select", layer.id);
+
+      li.appendChild(nameInput);
+      li.appendChild(deleteIcon);
+      this.layersDiv.appendChild(li);
+    }
+  }
+
+  private onRename(layer: BackgroundLayer, input: HTMLInputElement) {
+    const name = input.value.trim();
+    if (layer.name === name) return;
+    if (!name || name.length > input.maxLength || name.length < input.minLength) {
+      input.value = layer.name;
+      return;
     }
 
-    console.log(this.layersDiv);
-    console.log(background);
+    this.emit("background_layer_rename", [layer.id, name]);
   }
 }
 
